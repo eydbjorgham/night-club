a55e0e53f663bbaaaa0309df74cd7bc47d25ae89
Aljo0004 <aljo0004@stud.kea.dk>
2025-12-03 13:18:34 +0100

    Enhance VideoGallery styling with background color and clip-path for video

--- src/app/components/VideoGallery.jsx
+++ src/app/components/VideoGallery.jsx
@@ -7,12 +7,12 @@ import { IoMdArrowDropleft } from "react-icons/io";
 const VideoGallery = () => {
     return (
       <section className="mt-8 mb-16">
-        <div className="flex">
+        <div className="flex bg-(--tertiary-color) w-[60vw] justify-center m-auto">
           <video
             autoPlay
             loop
             muted
-            className="m-auto w-[60vw]"
+            className="m-auto w-[60vw] [clip-path:polygon(30px_0,100%_0,100%_calc(100%-30px),calc(100%-30px)_100%,0_100%,0_30px)]"
             src="/assets/media/video-crowd.mp4"
           ></video>
 
