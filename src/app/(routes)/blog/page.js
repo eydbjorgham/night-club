4dd15b0838dd18addcd25dac9bd0cc2001feb9d5
eydbjorgham <eyha0001@stud.kea.dk>
2025-12-03 12:00:43 +0100

    Blog page init

---
+++ src/app/(routes)/blog/page.js
@@ -0,0 +1,31 @@
+import Footer from "@/app/components/Footer";
+import Overskrift from "@/app/components/Overskrift";
+import Nav from "@/app/components/Nav";
+import { Ubuntu } from "next/font/google";
+
+const ubuntu = Ubuntu({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+});
+
+export default function BlogPage() {
+  return (
+    <>
+      <div className={ubuntu.className}>
+        <div className="bg-(--primary-color)">
+          <header>
+            <nav>
+              <Nav />
+            </nav>
+          </header>
+          <main>
+            <Overskrift text="Blog" />
+          </main>
+          <footer>
+            <Footer />
+          </footer>
+        </div>
+      </div>
+    </>
+  );
+}
