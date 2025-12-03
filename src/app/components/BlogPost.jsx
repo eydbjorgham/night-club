7ded683a3717b9a47e6451e080f747ddad51a043
Aljo0004 <aljo0004@stud.kea.dk>
2025-12-03 13:46:21 +0100

    Add BlogPost component and implement BlogPostPage layout

---
+++ src/app/components/BlogPost.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Image from "next/image";
+
+const BlogPost = () => {
+  return (
+    <section>
+      <img
+        src="/assets/content-img/blog_center3.jpg"
+        alt="blogcenter3img"
+        className="m-auto mt-16"
+      />
+      <h2 className="m-auto w-[80vw] text-white capitalize mt-8">
+        more than 20 years of night club
+      </h2>
+      <p className="m-auto w-[80vw] text-(--tertiary-color) mt-4">
+        BY: Admin/ 3 Comments / 16 Nov 2018
+      </p>
+      <p className="m-auto w-[80vw] text-white mt-8 pb-8">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia vel
+        repellendus, dignissimos deleniti corporis eius odit magni
+        exercitationem saepe alias impedit explicabo optio culpa debitis, rem
+        molestias animi ad totam. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Quia vel repellendus, dignissimos deleniti corporis
+        eius odit magni exercitationem saepe alias impedit explicabo optio culpa
+        debitis, rem molestias animi ad totam. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Quia vel repellendus, dignissimos deleniti
+        corporis eius odit magni exercitationem saepe alias impedit explicabo
+        optio culpa debitis, rem molestias animi ad totam. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Quia vel repellendus, dignissimos
+        deleniti corporis eius odit magni exercitationem saepe alias impedit
+        explicabo optio culpa debitis, rem molestias animi ad totam. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Quia vel repellendus,
+        dignissimos deleniti corporis eius odit magni exercitationem saepe alias
+        impedit explicabo optio culpa debitis, rem molestias animi ad totam.
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia vel
+        repellendus, dignissimos deleniti corporis eius odit magni
+        exercitationem saepe alias impedit explicabo optio culpa debitis, rem
+        molestias animi ad totam. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Quia vel repellendus, dignissimos deleniti corporis
+        eius odit magni exercitationem saepe alias impedit explicabo optio culpa
+        debitis, rem molestias animi ad totam.
+      </p>
+    </section>
+  );
+};
+
+export default BlogPost;
