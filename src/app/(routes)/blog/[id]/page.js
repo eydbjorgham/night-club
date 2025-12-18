309f0e657916dfc596920f6d64eaa3a3ec4c4101
eydbjorgham <eyha0001@stud.kea.dk>
2025-12-18 20:12:55 +0000

    Refactor page components to use a consistent background image style and improve layout structure

--- src/app/(routes)/blog/[id]/page.js
+++ src/app/(routes)/blog/[id]/page.js
@@ -15,31 +15,39 @@ export default async function BlogPostPage({ params }) {
   const { id } = await params;
   
   return (
-    <div className={ubuntu.className}>
-      <div className="bg-(--primary-color)">
-        <nav className="sticky top-0 z-50 bg-(--tertiary-color)">
-          <Nav />
-        </nav>
-        <section>
-          <div
-            className="mb-(--section-padding) bg-cover bg-center pt-6 pb-6"
-            style={{
-              backgroundImage:
-                "linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9)), url('/assets/bg/footerbg.jpg')",
-            }}
-          >
-            <Overskrift text="Blog post" />
-          </div>
+    <div
+      style={{
+        backgroundImage: "url('/assets/bg/pattern_bg.jpg')",
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
+      }}
+    >
+      <div className={ubuntu.className}>
+        <div className="bg-(--primary-color)">
+          <nav className="sticky top-0 z-50 bg-(--tertiary-color)">
+            <Nav />
+          </nav>
+          <section>
+            <div
+              className="mb-(--section-padding) bg-cover bg-center pt-6 pb-6"
+              style={{
+                backgroundImage:
+                  "linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9)), url('/assets/bg/footerbg.jpg')",
+              }}
+            >
+              <Overskrift text="Blog post" />
+            </div>
 
-          <BlogPost id={id} />
+            <BlogPost id={id} />
 
-          <Comments />
+            <Comments />
 
-          <LeaveComment />
-        </section>
-        <footer>
-          <Footer />
-        </footer>
+            <LeaveComment />
+          </section>
+          <footer>
+            <Footer />
+          </footer>
+        </div>
       </div>
     </div>
   );
